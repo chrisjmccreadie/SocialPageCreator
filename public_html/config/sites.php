097d1b55f30795e31a8a7bdaab1b0c4558e5fb92
Chris McCreadie <chris@cjmcsoftware.co.uk>
2011-08-29 10:33:27 +0100

    moved readme

--- public_html/config/sites.php
+++ public_html/config/sites.php
@@ -16,7 +16,15 @@ $sites[0]['headerimage'] = 'http://www.totemsoftware.co.uk/images/logo-software.
 $sites[0]['about'] = 'class/custom/aboutsite1.php';
 $sites[0]['css'] = 'css/layout.css';
 
-$sites[1]['title']  = 'example site2';
-$sites[1]['url'] = '127.0.1.1';
-$sites[1]['twitter'] = 'totemsoftwares';
+$sites[1]['title']  = 'example site';
+$sites[1]['url'] = 'totemspc.fluxflex.com';
+$sites[1]['twitter'] = 'totemsoftware';
+$sites[1]['twittercount'] = 5;
+$sites[1]['rss1'] = 'http://www.thetutorialblog.com/feed/';
+$sites[1]['rss2'] = 'totemsoftware';
+$sites[1]['youtubeurl'] = '95imNAIohTU,95imNAIohTU,95imNAIohTU,95imNAIohTU,95imNAIohTU,95imNAIohTU';
+$sites[1]['customhtml1'] = 'this could be an iframe or any custom code';
+$sites[1]['headerimage'] = 'http://www.totemsoftware.co.uk/images/logo-software.png';
+$sites[1]['about'] = 'class/custom/aboutsite1.php';
+$sites[1]['css'] = 'css/layout.css';
 ?>
